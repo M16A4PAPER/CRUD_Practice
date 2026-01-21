6a6c18c587171e0a7dcf1bb5340ae67cad9b4937
M16A4PAPER <m16a4paper@gmail.com>
2026-01-21 19:59:54 +0500

    Structure Finalization

---
+++ CRUD_Practice/CRUD_Practice.DBMySql/Repositories/DepartmentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_Practice.Models.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Practice.DBMySql.Repositories
+{
+    public class DepartmentsRepository : IDepartmentsRepository
+    {
+    }
+}
